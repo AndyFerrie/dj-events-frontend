d7ceddcc1fdec3519dbbfbfe3cebf06fb809e46e
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2023-09-19 09:21:23 +0100

    Updates event page with dynamic content

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,11 +1,47 @@
 import Layout from "@/components/Layout"
 import { API_URL } from "@/config"
+import styles from "@/styles/Event.module.css"
+import Link from "next/link"
+import Image from "next/image"
+import { FaPencilAlt, FaTimes } from "react-icons/fa"
 
 export default function EventPage({event}) {
+  const deleteEvent = (e) => {
+    console.log('delete')
+  }
 
   return (
     <Layout>
-        <h1>{event.name}</h1>
+        <div className={styles.event}>
+
+          <div className={styles.controls}>
+            <Link legacyBehavior href={`/events/edit/${event.id}`}>
+              <a>
+                <FaPencilAlt /> Edit Event
+              </a>
+            </Link>
+            <a href="#" className={styles.delete} onClick={deleteEvent}> 
+              <FaTimes /> Delete Event
+            </a>
+          </div>
+
+          <span>
+            {event.date} at {event.time}
+          </span>
+          <h1>{event.name}</h1>
+          {event.image && (
+            <div className={styles.image}>
+              <Image src={event.image} width={960} height={600}></Image>
+            </div>
+          )}
+
+          <h3>Peformers:</h3>
+          <p>{event.performers}</p>
+          <h3>Description</h3>
+          <p>{event.description}</p>
+          <h3>Venue: {event.venue}</h3>
+          <p>{event.address}</p>
+        </div>
     </Layout>
   )
 }
