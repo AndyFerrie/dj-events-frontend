117fbe75f749b698e85d79dd00ddbb2e5712def5
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2023-09-24 18:12:45 +0100

    Adds pagination to events

---
+++ components/Pagination.js
@@ -0,0 +1,25 @@
+import Link from "next/link"
+import { PER_PAGE } from "@/config"
+
+export default function Pagination({page, total}) {
+
+    
+
+    const lastPage = Math.ceil(total / PER_PAGE)
+
+    return (
+        <>
+            {page > 1 && (
+                <Link legacyBehavior href={`/events?page=${page - 1}`}>
+                    <a className='btn-secondary'>Prev</a>
+                </Link>
+            )}
+
+            {page < lastPage && (
+                <Link legacyBehavior href={`/events?page=${Number(page) + 1}`}>
+                    <a className='btn-secondary'>Next</a>
+                </Link>
+            )}
+        </>
+    )
+}
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,29 +1,36 @@
 import Layout from "@/components/Layout"
 import EventItem from "@/components/EventItem"
 import { API_URL } from "@/config/index"
+import { PER_PAGE } from "@/config/index"
+import Pagination from "@/components/Pagination"
 
-export default function EventsPage({events}) {
+export default function EventsPage({events, page, total}) {
+
   return (
+    console.log(page),
     <Layout>
       <h1>Events</h1>
       {events.length === 0 && <h3>No events to show</h3>}
 
       {events.map(event => (
         <EventItem key={event.id} event={event.attributes} />
-      )
-    )}
+      ))}
+
+      <Pagination page={page} total={total}/>
 
     </Layout>
   )
 }
 
-export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/api/events?populate=*&_sort=date:ASC`)
-  const results = await res.json()
+export async function getServerSideProps({query: {page = 1}}) {
+
+  const eventRes = await fetch(`${API_URL}/api/events?populate=*&_sort=date:ASC&pagination[page]=${page}&pagination[pageSize]=${PER_PAGE}`)
+  const results = await eventRes.json()
   const events = results.data
 
+  const total = results.meta.pagination.total
+
   return {
-    props: {events},
-    revalidate: 1
+    props: {events, page, total},
   }
 }
