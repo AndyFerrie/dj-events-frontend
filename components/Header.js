3668b1b570e15082c7b3971444ab0aa0e68ee3d6
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2023-09-25 07:16:32 +0100

    Adds login button to header

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import Link from "next/link"
 import Search from "./Search"
 import styles from "../styles/Header.module.css"
+import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa'
 
 export default function Header() {
   return (
@@ -23,6 +24,13 @@ export default function Header() {
                 <li>
                     <Link legacyBehavior href='/events/add'>
                         <a>Add Event</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link legacyBehavior href='/account/login'>
+                        <a className="btn-secondary btn-icon">
+                            <FaSignInAlt /> Login
+                        </a>
                     </Link>
                 </li>
             </ul>
