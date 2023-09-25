7b5741530a4b40f14af8434a8aa349653404d080
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2023-09-25 08:50:50 +0100

    Adds authcontext

--- components/Header.js
+++ components/Header.js
@@ -1,40 +1,65 @@
 import Link from "next/link"
+import { useContext } from "react"
 import Search from "./Search"
 import styles from "../styles/Header.module.css"
+import AuthContext from "@/context/AuthContext"
 import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa'
 
 export default function Header() {
-  return (
-    <header className={styles.header}>
-        <div className={styles.logo}>
-            <Link legacyBehavior href='/'>
-                <a>DJ Events</a>
-            </Link>
-        </div>
+    const  {user, logout} = useContext(AuthContext)
 
-        <Search />
+    return (
+        <header className={styles.header}>
+            <div className={styles.logo}>
+                <Link legacyBehavior href='/'>
+                    <a>DJ Events</a>
+                </Link>
+            </div>
 
-        <nav>
-            <ul>
-                <li>
-                    <Link legacyBehavior href='/events'>
-                        <a>Events</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link legacyBehavior href='/events/add'>
-                        <a>Add Event</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link legacyBehavior href='/account/login'>
-                        <a className="btn-secondary btn-icon">
-                            <FaSignInAlt /> Login
-                        </a>
-                    </Link>
-                </li>
-            </ul>
-        </nav>
-    </header>
-  )
+            <Search />
+
+            <nav>
+                <ul>
+                    <li>
+                        <Link legacyBehavior href='/events'>
+                            <a>Events</a>
+                        </Link>
+                    </li>
+                    {user ? (
+                        // If logged in 
+                            <>
+                                <li>
+                                    <Link legacyBehavior href='/events/add'>
+                                        <a>Add Event</a>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link legacyBehavior href='/account/dashboard'>
+                                        <a>Dashboard</a>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <button onClick={() => logout()} className="btn-secondary btn-icon">
+                                        <FaSignOutAlt/> Logout
+                                    </button>
+                                </li>
+                            </>
+                        ) : (
+                        // If logged out
+                            <>
+                                <li>
+                                    <Link legacyBehavior href='/account/login'>
+                                        <a className="btn-secondary btn-icon">
+                                            <FaSignInAlt /> Login
+                                        </a>
+                                    </Link>
+                                </li>
+                            </>
+                        )
+                    }
+
+                </ul>
+            </nav>
+        </header>
+    )
 }
