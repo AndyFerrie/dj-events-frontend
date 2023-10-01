376431b7c08f51d56d270f5949739d6184550074
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2023-10-01 08:58:40 +0100

    Adds token to image upload

--- components/ImageUpload.js
+++ components/ImageUpload.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { API_URL } from "@/config";
 import styles from "@/styles/Form.module.css";
 
-export default function ImageUpload({ eventId, imageUploaded }) {
+export default function ImageUpload({ eventId, imageUploaded, token }) {
     const [image, setImage] = useState(null);
 
     const handleSubmit = async (e) => {
@@ -17,6 +17,9 @@ export default function ImageUpload({ eventId, imageUploaded }) {
 
         const res = await fetch(`${API_URL}/api/upload`, {
             method: "POST",
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
             body: formData,
         });
 
